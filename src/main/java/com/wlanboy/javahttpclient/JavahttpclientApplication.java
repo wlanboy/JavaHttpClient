3adc39cccefa095f0b0e71f552565e64abe34740
wlanboy <me@wlanboy.com>
2021-06-27 22:22:44 +0200

    added application classes

---
+++ src/main/java/com/wlanboy/javahttpclient/JavahttpclientApplication.java
@@ -0,0 +1,13 @@
+package com.wlanboy.javahttpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavahttpclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavahttpclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wlanboy/javahttpclient/JavahttpclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlanboy.javahttpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavahttpclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
